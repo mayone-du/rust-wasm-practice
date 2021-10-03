f035e735d1fe9610bd361234ce6f1456af552c32
mayone-du <cocomayo1201@icloud.com>
2021-10-03 17:57:03 +0900

    Add add_one simple fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add_one(num: i32) -> i32 {
+    num + 1
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_add_one() {
+        assert_eq!(add_one(3), 4);
     }
 }
